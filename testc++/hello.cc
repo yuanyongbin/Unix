9e14d5c5d4f71af42daae37a06f58878dc445a95
root <root@localhost.localdomain>
2017-02-03 15:20:16 +0800

    test

---
+++ testc++/hello.cc
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "hello world" << endl;
+	cout << "this is my first c++ program.\n";
+}
---
+++ testc++/test.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	int n = 100;
+	char ch = 'a';
+	char cha[100] = "char\0name";
+	cout << cha << endl;
+
+}
